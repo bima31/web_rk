fda3811b84de3be8dbae6ef27773351a79d73930
bima31 <acchanbima@gmail.com>
2024-05-15 01:44:54 +0700

    init first

---
+++ dist/js/func_scrollreveal.js
@@ -0,0 +1,21 @@
+
+ScrollReveal().reveal('.section', {
+    delay: 400,
+});
+
+ScrollReveal().reveal('#store1', {
+    delay: 300,
+});
+ScrollReveal().reveal('#store2', {
+    delay: 600,
+});
+ScrollReveal().reveal('#store3', {
+    delay: 900,
+});
+ScrollReveal().reveal('#store4', {
+    delay: 1200,
+});
+
+ScrollReveal().reveal('#gallery', {
+    delay: 500,
+});
